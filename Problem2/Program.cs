fdd0e54de6a7e708f322685755a125bd76b3a865
atanas-georgiev <atanasgeorgiev83@gmail.com>
2015-02-01 20:35:06 +0200

    Initial

---
+++ Problem2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string x = Console.ReadLine();
+            double sum = 0, cnt = 0, i = 0;
+            
+
+            foreach (var ch in x)
+            {
+                if ((i % 2 == 0) && char.IsDigit(ch))
+                {
+                    cnt++;
+                    sum += (double.Parse(ch.ToString()) - double.Parse("0"));
+                }
+                i++;
+            }
+            Console.WriteLine(cnt + " " + sum);
+        }
+    }
+}
