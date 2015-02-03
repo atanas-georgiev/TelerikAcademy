e3c4e5a07d5e4e3c097042f708363843d4739112
atanas-georgiev <atanasgeorgiev83@gmail.com>
2015-02-03 15:49:54 +0200

    Initial

---
+++ Problem04DownloadFile/DownloadFile.cs
@@ -0,0 +1,17 @@
+﻿//Problem 4. Download file
+
+//Write a program that downloads a file from Internet (e.g. Ninja image) and stores it the current directory.
+//Find in Google how to download files in C#.
+//Be sure to catch all exceptions and to free any used resources in the finally block.
+
+using System;
+
+namespace Problem04DownloadFile
+{
+    class DownloadFile
+    {
+        static void Main()
+        {
+        }
+    }
+}
