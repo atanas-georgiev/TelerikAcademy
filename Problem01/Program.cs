b01fdd845b8f03c8de5f45813a12cb50785a0091
atanas-georgiev <atanasgeorgiev83@gmail.com>
2015-02-23 12:42:41 +0200

    Updated

--- Problem01/Program.cs
+++ Problem01/Program.cs
@@ -8,100 +8,8 @@ namespace Problem01
 {
     class Program
     {
-        static string ConvertNumbericSystem(string value, int base1, int base2)
+        static string ConvertNumbericSystem(ulong value)
         {
-            ulong decRepresentation = 0;
-
-            // base 1 to decimal system
-            for (int i = value.Length - 1; i >= 0; i--)
-            {
-                if (Char.IsDigit(value[i]))
-                {
-                    decRepresentation += (ulong)((value[i] - '0') * Math.Pow(base1, value.Length - i - 1));
-                }
-                else
-                {
-                    int num = 0;
-
-                    switch (value[i])
-                    {
-                        case 'a':
-                        case 'A':
-                            num = 10;
-                            break;
-                        case 'b':
-                        case 'B':
-                            num = 11;
-                            break;
-                        case 'c':
-                        case 'C':
-                            num = 12;
-                            break;
-                        case 'd':
-                        case 'D':
-                            num = 13;
-                            break;
-                        case 'e':
-                        case 'E':
-                            num = 14;
-                            break;
-                        case 'f':
-                        case 'F':
-                            num = 15;
-                            break;
-                        default:
-                            break;
-                    }
-
-                    decRepresentation += (ulong)(num * Math.Pow(base1, value.Length - i - 1));
-                }
-            }
-
-            ulong remainder;
-            string result = string.Empty;
-
-            while (decRepresentation > 0)
-            {
-                remainder = (ulong)(decRepresentation % (ulong)base2);
-                decRepresentation /= (ulong)base2;
-
-                if (remainder < 10)
-                {
-                    result = remainder.ToString() + result;
-                }
-                else
-                {
-                    switch (remainder)
-                    {
-                        case 10:
-                            result = "A" + result;
-                            break;
-                        case 11:
-                            result = "B" + result;
-                            break;
-                        case 12:
-                            result = "C" + result;
-                            break;
-                        case 13:
-                            result = "D" + result;
-                            break;
-                        case 14:
-                            result = "E" + result;
-                            break;
-                        case 15:
-                            result = "F" + result;
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }
-            return result;
-        }
-
-        static void Main()
-        {
-
             string[] dict = new string[9];
 
             dict[0] = @"LON+";
@@ -114,16 +22,25 @@ namespace Problem01
             dict[7] = @"/TEL";
             dict[8] = @"<<DON";
 
-            string input = Console.ReadLine();
-            string res = ConvertNumbericSystem(input, 10, 9);
-            string res1 = "";
-
-            foreach (char ch in res)
+            List<string> tresNumber = new List<string>();
+            do
             {
-                res1 += dict[ch - '0'];
-            }
+                tresNumber.Add(dict[value % 9]);
 
-            Console.WriteLine(res1);
+                value /= 9;
+            }
+            while (value != 0);
+
+            tresNumber.Reverse();
+            return string.Join(string.Empty, tresNumber);
+        }
+
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string res = ConvertNumbericSystem(ulong.Parse(input));
+
+            Console.WriteLine(res);
         }
     }
 }
--- Problem02/Program.cs
+++ Problem02/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace Problem02
 {
@@ -20,17 +21,21 @@ namespace Problem02
 
             int cycle = 1;
 
-            while (cycle < cages.Count)
+            // 1.	If there are less than i cages, the factory stops the multiplication process.
+            while (cycle <= cages.Count)
             {
-                int sum = 0;
-                int sum1 = 0;
-                int product = 1;
+                BigInteger sum = 0;
+                BigInteger sum1 = 0;
+                BigInteger product = 1;
 
+                // 2.	The factory gets the first i cages and calculate the sum s of all bunnies in them. 
                 for (int i = 0; i < cycle; i++)
                 {
                     sum += cages[i];
                 }
 
+
+                // 3.	If there are less than s cages after the i-th one, the factory stops the multiplication process.
                 if (sum > cages.Count - cycle)
                 {
                     break;
@@ -38,21 +43,22 @@ namespace Problem02
 
                 sum1 = 0;
 
+                // 4.	The factory gets the next s cages after the i-th one and calculates the sum and product of all bunnies in them. 
                 for (int i = cycle; i < cycle + sum; i++)
                 {
                     sum1 += cages[i];
                     product *= cages[i];
                 }
 
+                // 5.	These sum and product are concatenated as next cages. All cages that were not included in the calculations are simply appended to next.
                 string result = sum1.ToString() + product.ToString();
-
-                for (int i = cycle + sum; i < cages.Count; i++)
+                for (int i = cycle + (int)sum; i < cages.Count; i++)
                 {
                     result += cages[i].ToString();
                 }
 
+                // 6.	The factory gets several empty cages then gets the digits of next and for each digit puts the same amount of bunnies in each cell. If the digit is 1 or 0, the digit is ignored.
                 cages.Clear();
-
                 foreach (char ch in result)
                 {
                     if (ch != '0' && ch != '1')
@@ -61,6 +67,7 @@ namespace Problem02
                     }
                 }
 
+                // 7.	Step 1 is repeated for the newly generated cages with bunnies.
                 cycle++;
             }
 
