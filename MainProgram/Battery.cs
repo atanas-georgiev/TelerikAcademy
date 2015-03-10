cc3ca78b35c7f66350fb6b6d02d6fc1d5e8808dc
atanas-georgiev <atanasgeorgiev83@gmail.com>
2015-03-10 14:51:19 +0200

    Initial

---
+++ MainProgram/Battery.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MainProgram
+{
+    class Battery
+    {
+        // Constant Fields
+        private const int MaxLengthModel = 20;
+
+        // Fields
+        private string model;
+        private double idleTime;
+        private double hoursTime;
+        private BatteryType type;
+
+        // Constructors
+        public Battery() { }
+
+        public Battery(string model = "NoName", double idleTime = 1, double hoursTime = 1, BatteryType type = BatteryType.LiIon)
+        {
+            this.Model = model;
+            this.idleTime = idleTime;
+            this.hoursTime = hoursTime;
+            this.Type = type;
+        }
+
+        // Enums
+        public enum BatteryType
+        {
+            LiIon,
+            NiMH,
+            NiCd
+        }
+
+        // Properties
+        public BatteryType Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set 
+            {
+                if (value.Length > MaxLengthModel)
+                    throw new ArgumentException("Name is too long!");
+                model = value; 
+            }
+        }
+
+        public double IdleTime
+        {
+            get { return idleTime; }
+            set
+            {
+                if (value < 1 || value > 1000)
+                    throw new ArgumentException("Invalid value!");
+                idleTime = value;
+            }
+        }
+
+        public double HoursTime
+        {
+            get { return hoursTime; }
+            set
+            {
+                if (value < 1 || value > 1000)
+                    throw new ArgumentException("Invalid value!");
+                hoursTime = value;
+            }
+        }
+    }
+}
---
+++ MainProgram/Call.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MainProgram
+{
+    class Call
+    {
+        // Fields
+        private DateTime dateTime;
+        private string number;
+        private int duration;
+
+        // Constructors
+        public Call() { }
+        public Call(DateTime dt, string number, int duration)
+        {
+            this.DateTimeValue = dt;
+            this.Number = number;
+            this.Duration = duration;
+        }
+
+        // Properties
+        public DateTime DateTimeValue
+        {
+            get { return dateTime; }
+            set 
+            {
+                try
+                {
+                    dateTime = DateTime.Parse(value.ToString());
+                }
+                catch
+                {
+                    throw new ArgumentException ("Not valid date/time entered!");
+                }                
+            }
+        }
+
+        public string Number
+        {
+            get { return number; }
+            set 
+            {
+                if (CheckNumber(value))
+                {
+                    number = value;
+                }
+                else
+                {
+                    throw new ArgumentException ("Not valid number entered!");
+                }                
+            }
+        }
+
+        public int Duration
+        {
+            get { return duration; }
+            set
+            {
+                if (value > 0)
+                {
+                    duration = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Not valid duration entered!");
+                }
+            }
+        }
+
+        private static bool CheckNumber(string strPhoneNumber)
+        {
+           string MatchPhoneNumberPattern = @"^\+?(\d[\d-. ]+)?(\([\d-. ]+\))?[\d-. ]+\d$";
+           if (strPhoneNumber!= null) return Regex.IsMatch(strPhoneNumber, MatchPhoneNumberPattern );
+           else return false;
+        }
+    }
+}
