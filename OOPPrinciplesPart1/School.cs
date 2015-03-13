5e5e4bd02009d43d39a4a8e516941a072bc87ecc
atanas-georgiev <atanasgeorgiev83@gmail.com>
2015-03-13 18:48:14 +0200

    Updated

---
+++ OOPPrinciplesPart1/School.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOPPrinciplesPart1
+{
+    class School
+    {
+        public List<SchoolClass> Classes { get; set; }
+
+        public School() { }
+    }
+}
---
+++ OOPPrinciplesPart1/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOPPrinciplesPart1
+{
+    class Student : Person, IComment
+    {
+        public string Comment { get; set; }
+        public int ClassNumber { get; set; }
+        public Student() { }
+    }
+}
