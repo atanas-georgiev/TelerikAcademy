929072e1bb9b73e0baf3997bde3e8b334eed3404
atanas-georgiev <atanasgeorgiev83@gmail.com>
2015-03-24 16:14:57 +0200

    Updated

--- Problem03RangeExceptions/Program.cs
+++ Problem03RangeExceptions/Program.cs
@@ -10,6 +10,25 @@ namespace Problem03RangeExceptions
     {
         static void Main(string[] args)
         {
+            try
+            {
+                throw new InvalidRangeException<int>("Invalid input!", 2, 50);
+            }
+            catch (InvalidRangeException<int> e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("\n\n");
+
+            try
+            {
+                throw new InvalidRangeException<DateTime>("Invalid date!", new DateTime(1981, 1, 1), DateTime.Now);
+            }
+            catch (InvalidRangeException<DateTime> e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
