285ea2b2e060aed8a5588876575c7b25e22fcafa
atanas-georgiev <atanasgeorgiev83@gmail.com>
2015-03-24 12:51:30 +0200

    Updated

---
+++ Problem02_AcademyRPG/AcademyRPG/Giant.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AcademyRPG
+{
+    public class Giant : Character, IFighter, IGatherer
+    {
+        private bool isStoneGathered;
+        private int attackPoints;
+        public Giant(string name, Point position)
+            : base(name, position, 0)
+        {
+            this.HitPoints = 200;
+            this.isStoneGathered = false;
+            this.attackPoints = 150;
+        }
+
+        public int AttackPoints
+        {
+            get { return this.attackPoints; }
+        }
+
+        public int DefensePoints
+        {
+            get { return 80; }
+        }
+
+        public int GetTargetIndex(List<WorldObject> availableTargets)
+        {
+            for (int i = 0; i < availableTargets.Count; i++)
+            {
+                if (availableTargets[i].Owner != 0)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public bool TryGather(IResource resource)
+        {
+            if (resource.Type == ResourceType.Stone)
+            {
+                if (this.isStoneGathered == false)
+                {
+                    this.attackPoints += 100;
+                    this.isStoneGathered = true;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Problem02_AcademyRPG/AcademyRPG/Rock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AcademyRPG
+{
+    public class Rock : StaticObject, IResource
+    {
+        protected int quantity { get; private set; }
+
+        public ResourceType Type
+        {
+            get
+            {
+                return ResourceType.Stone;
+            }
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return this.quantity;
+            }
+        }
+
+        public Rock(Point position, int hitPoints)
+            : base(position)
+        {
+            this.quantity = hitPoints / 2;
+            this.HitPoints = hitPoints;            
+        }
+    }
+}
+
