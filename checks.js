78290850d9d90bb3df1702853ddbc817b83f09c6
atanas-georgiev <atanasgeorgiev83@gmail.com>
2015-07-08 16:19:49 +0300

    updated

--- checks.js
+++ checks.js
@@ -1,110 +1,97 @@
-// Integer
-function isInteger(value) {
-    'use strict';
-    return typeof value === "number" && value === +value && Math.round(value) === value;
-}
+var validators = {
+    // Undefined
+    checkUndefinedAndThrow: function(value) {
+        if (value === undefined) {
+            throw new Error(value + ' is undefined');
+        }
+    },
+    // Integer
+    isInteger: function(value) {
+        this.checkUndefinedAndThrow(value);
+        return typeof value === "number" && value === +value && Math.round(value) === value;
+    },
+    checkIntegerAndThrow: function(value) {
+        this.checkUndefinedAndThrow(value);
+        if (!this.isInteger(value)) {
+            throw new Error(value + ' not a integer!');
+        }
+    },
+    // Positive integer
+    isPositiveInteger: function(value) {
+        this.checkUndefinedAndThrow(value);
+        return typeof value === "number" && value === +value && Math.round(value) === value && value >= 0;
+    },
+    checkPositiveIntegerAndThrow: function(value) {
+        this.checkUndefinedAndThrow(value);
+        if (!this.isPositiveInteger(value)) {
+            throw new Error(value + ' not a positive integer!');
+        }
+    },
+    // Float
+    isFloat: function(value) {
+        this.checkUndefinedAndThrow(value);
+        return typeof value === "number" && value === +value && Math.round(value) !== value;
+    },
+    checkFloatAndThrow: function(value) {
+        this.checkUndefinedAndThrow(value);
+        if (!this.isFloat(value)) {
+            throw new Error(value + ' not a float!');
+        }
+    },
+    // Number
+    isNumber: function(value) {
+        this.checkUndefinedAndThrow(value);
+        return !isNaN(parseFloat(value)) && isFinite(value);
+    },
+    checkNumberAndThrow: function(value) {
+        this.checkUndefinedAndThrow(value);
+        if (!this.isNumber(value)) {
+            throw new Error(value + ' not a number!');
+        }
+    },
+    // String
+    isString: function(value) {
+        this.checkUndefinedAndThrow(value);
+        return (typeof value === 'string' || value instanceof String);
+    },
 
-function checkIntegerAndThrow(value) {
-    'use strict';
-    if (!isInteger(value)) {
-        throw new Error(value + ' not a integer!');
+    checkStringAndThrow: function(value) {
+        this.checkUndefinedAndThrow(value);
+        if (!this.isString(value)) {
+            throw new Error(value + ' not a string!');
+        }
+    },
+    // Boolean
+    isBoolean: function(value) {
+        this.checkUndefinedAndThrow(value);
+        return typeof value === 'boolean' || value === 'true' || value === 'false';
+    },
+    checkBooleanAndThrow: function(value) {
+        if (!this.isBoolean(value)) {
+            this.checkUndefinedAndThrow(value);
+            throw new Error(value + ' not a boolean!');
+        }
+    },
+    // Array
+    isArray: function(value) {
+        this.checkUndefinedAndThrow(value);
+        return Array.isArray(value);
+    },
+    checkArrayAndThrow: function(value) {
+        this.checkUndefinedAndThrow(value);
+        if (!this.isArray(value)) {
+            throw new Error(value + ' not a array!');
+        }
+    },
+    // Object
+    isObject: function(value) {
+        this.checkUndefinedAndThrow(value);
+        return Object.prototype.toString.call(value) === '[object Object]';
+    },
+    checkObjectAndThrow: function(value) {
+        this.checkUndefinedAndThrow(value);
+        if (!this.isObject(value)) {
+            throw new Error(value + ' not a object!');
+        }
     }
-}
-
-// Positive integer
-function isPositiveInteger(value) {
-    'use strict';
-    return typeof value === "number" && value === +value && Math.round(value) === value && value >= 0;
-}
-
-function checkPositiveIntegerAndThrow(value) {
-    'use strict';
-    if (!isPositiveInteger(value)) {
-        throw new Error(value + ' not a positive integer!');
-    }
-}
-
-// Float
-function isFloat(value) {
-    'use strict';
-    return typeof value === "number" && value === +value && Math.round(value) !== value;
-}
-
-function checkFloatAndThrow(value) {
-    'use strict';
-    if (!isFloat(value)) {
-        throw new Error(value + ' not a float!');
-    }
-}
-
-// Number
-function isNumber(value) {
-    'use strict';
-    return !isNaN(parseFloat(value)) && isFinite(value);
-}
-
-function checkNumberAndThrow(value) {
-    'use strict';
-    if (!isNumber(value)) {
-        throw new Error(value + ' not a number!');
-    }
-}
-
-// String
-function isString(value) {
-    'use strict';
-    return (typeof value === 'string' || value instanceof String);
-}
-
-function checkStringAndThrow(value) {
-    'use strict';
-    if (!isString(value)) {
-        throw new Error(value + ' not a string!');
-    }
-}
-
-// Boolean
-function isBoolean(value) {
-    'use strict';
-    return typeof value === 'boolean' || value === 'true' || value === 'false';
-}
-
-function checkBooleanAndThrow(value) {
-    'use strict';
-    if (!isBoolean(value)) {
-        throw new Error(value + ' not a boolean!');
-    }
-}
-
-// Array
-function isArray(value) {
-    'use strict';
-    return Array.isArray(value);
-}
-
-function checkArrayAndThrow(value) {
-    'use strict';
-    if (!isArray(value)) {
-        throw new Error(value + ' not a array!');
-    }
-}
-
-// Object
-function isObject(value) {
-    'use strict';
-    return Object.prototype.toString.call(value) === '[object Object]';
-}
-
-function checkObjectAndThrow(value) {
-    'use strict';
-    if (!isObject(value)) {
-        throw new Error(value + ' not a object!');
-    }
-}
-
-
-console.log(isObject(1.11));
-console.log(isObject({}));
-console.log(isObject("true"));
-console.log(isObject());
-console.log(isObject(null));
+};
