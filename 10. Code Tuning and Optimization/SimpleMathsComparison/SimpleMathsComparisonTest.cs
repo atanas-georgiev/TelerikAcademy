6d3ad1bcb4bf2f81485d05fe5c93016206fc26e3
atanas-georgiev <atanasgeorgiev83@gmail.com>
2015-07-30 11:54:57 +0300

    Homework complete.

---
+++ 10. Code Tuning and Optimization/SimpleMathsComparison/SimpleMathsComparisonTest.cs
@@ -0,0 +1,130 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SimpleMathsComparisonTest.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The simple maths comparison test.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SimpleMathsComparison
+{
+    using System;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// The simple maths comparison test.
+    /// </summary>
+    internal class SimpleMathsComparisonTest
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        internal static void Main()
+        {
+            Console.WriteLine("------------------------------- INT -------------------------------");
+            var x1 = 0;
+            ExecuteMathTests(x1);
+
+            Console.WriteLine("------------------------------- LONG -------------------------------");
+            long x2 = 0;
+            ExecuteMathTests(x2);
+
+            Console.WriteLine("------------------------------- FLOAT -------------------------------");
+            float x3 = 0;
+            ExecuteMathTests(x3);
+
+            Console.WriteLine("------------------------------- DOUBLE -------------------------------");
+            double x4 = 0;
+            ExecuteMathTests(x4);
+
+            Console.WriteLine("------------------------------- DECIMAL -------------------------------");
+            decimal x5 = 0;
+            ExecuteMathTests(x5);
+        }
+
+        /// <summary>
+        /// The display execution time.
+        /// </summary>
+        /// <param name="action">
+        /// The action.
+        /// </param>
+        /// <exception cref="Exception">
+        /// A delegate callback throws an exception.
+        /// </exception>
+        private static void DisplayExecutionTime(Action action)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            action();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+        }
+
+        /// <summary>
+        /// The execute math tests.
+        /// </summary>
+        /// <param name="a">
+        /// The a.
+        /// </param>
+        private static void ExecuteMathTests(dynamic a)
+        {
+            const int LOOP_COUNT = 5000000;
+
+            Console.Write("Add: ");
+            DisplayExecutionTime(
+                () =>
+                    {
+                        a = a + 0;
+                        for (var i = 0; i < LOOP_COUNT; i++)
+                        {
+                            a = a + 1;
+                        }
+                    });
+
+            Console.Write("Subtract: ");
+            DisplayExecutionTime(
+                () =>
+                    {
+                        a = a + 5000000;
+                        for (var i = 0; i < LOOP_COUNT; i++)
+                        {
+                            a = a - 1;
+                        }
+                    });
+
+            Console.Write("Increment: ");
+            DisplayExecutionTime(
+                () =>
+                    {
+                        a = a + 0;
+                        for (var i = 0; i < LOOP_COUNT; i++)
+                        {
+                            a++;
+                        }
+                    });
+
+            Console.Write("Multiply: ");
+            DisplayExecutionTime(
+                () =>
+                    {
+                        a = a + 1;
+                        for (var i = 0; i < LOOP_COUNT; i++)
+                        {
+                            a = a * 1;
+                        }
+                    });
+
+            Console.Write("Divide: ");
+            DisplayExecutionTime(
+                () =>
+                    {
+                        a = a + 1;
+                        for (var i = 0; i < LOOP_COUNT; i++)
+                        {
+                            a = a / 1;
+                        }
+                    });
+        }
+    }
+}
---
+++ 10. Code Tuning and Optimization/SolarSystem/PropertyHolder.cs
@@ -0,0 +1,89 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PropertyHolder.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The property holder.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Surfaces
+{
+    using System.Windows;
+
+    /// <summary>
+    /// The property holder.
+    /// </summary>
+    /// <typeparam name="PropertyType">
+    /// </typeparam>
+    /// <typeparam name="HoldingType">
+    /// </typeparam>
+    public class PropertyHolder<PropertyType, HoldingType>
+        where HoldingType : DependencyObject
+    {
+        /// <summary>
+        /// The _property.
+        /// </summary>
+        private readonly DependencyProperty _property;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PropertyHolder{PropertyType,HoldingType}"/> class.
+        /// </summary>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        /// <param name="defaultValue">
+        /// The default value.
+        /// </param>
+        /// <param name="propertyChangedCallback">
+        /// The property changed callback.
+        /// </param>
+        public PropertyHolder(string name, PropertyType defaultValue, PropertyChangedCallback propertyChangedCallback)
+        {
+            this._property = DependencyProperty.Register(
+                name, 
+                typeof(PropertyType), 
+                typeof(HoldingType), 
+                new PropertyMetadata(defaultValue, propertyChangedCallback));
+        }
+
+        /// <summary>
+        /// Gets the property.
+        /// </summary>
+        public DependencyProperty Property
+        {
+            get
+            {
+                return this._property;
+            }
+        }
+
+        /// <summary>
+        /// The get.
+        /// </summary>
+        /// <param name="obj">
+        /// The obj.
+        /// </param>
+        /// <returns>
+        /// The <see cref="PropertyType"/>.
+        /// </returns>
+        public PropertyType Get(HoldingType obj)
+        {
+            return (PropertyType)obj.GetValue(this._property);
+        }
+
+        /// <summary>
+        /// The set.
+        /// </summary>
+        /// <param name="obj">
+        /// The obj.
+        /// </param>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        public void Set(HoldingType obj, PropertyType value)
+        {
+            obj.SetValue(this._property, value);
+        }
+    }
+}
