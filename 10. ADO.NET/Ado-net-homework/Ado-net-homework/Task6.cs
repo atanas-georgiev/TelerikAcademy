9aa4f43a78706a0ba7100d705a98d6f5b6f4136e
Atanas Georgiev <atanasgeorgiev83@gmail.com>
2015-10-11 19:36:11 +0300

    Homework complete.

---
+++ 10. ADO.NET/Ado-net-homework/Ado-net-homework/Task6.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Task6.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The task 6.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Ado_net_homework
+{
+    using System;
+    using System.Data.OleDb;
+
+    /// <summary>
+    ///     The task 6.
+    /// </summary>
+    internal class Task6
+    {
+        /// <summary>
+        ///     The connection string.
+        /// </summary>
+        private const string ConnectionString =
+            "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=data.xls;Extended Properties = \"Excel 8.0;HDR=Yes;IMEX=1\";";
+
+        /// <summary>
+        ///     Write a program that reads your MS Excel file through the OLE DB data provider and displays the name and score row
+        ///     by row.
+        /// </summary>
+        public static void Execute()
+        {
+            var con = new OleDbConnection { ConnectionString = ConnectionString };
+            con.Open();
+
+            using (con)
+            {
+                Console.WriteLine("Name and score of the categories:");
+                var cmdAllScores = new OleDbCommand("SELECT Name, Score FROM [Sheet1$]", con);
+                var reader = cmdAllScores.ExecuteReader();
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        var name = (string)reader[0];
+                        var score = (double)reader[1];
+                        Console.WriteLine(name + " ---> " + score);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 10. ADO.NET/Ado-net-homework/Ado-net-homework/Task8.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Task8.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The task 8.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Ado_net_homework
+{
+    using System;
+    using System.Data.SqlClient;
+
+    /// <summary>
+    ///     The task 8.
+    /// </summary>
+    internal class Task8
+    {
+        /// <summary>
+        ///     The connection string.
+        /// </summary>
+        private const string ConnectionString = "Server=.; database=Northwind; Integrated Security=SSPI";
+
+        /// <summary>
+        ///     Write a program that reads a string from the console and finds all products that contain this string.
+        /// </summary>
+        public static void Execute()
+        {
+            var con = new SqlConnection { ConnectionString = ConnectionString };
+            con.Open();
+
+            using (con)
+            {
+                Console.WriteLine("Enter some string:");
+                var data = Console.ReadLine();
+
+                var cmdAllProducts = new SqlCommand(
+                    "SELECT ProductName FROM Products WHERE ProductName LIKE @name", 
+                    con);
+                cmdAllProducts.Parameters.AddWithValue("@name", "%" + data + "%");
+                var reader = cmdAllProducts.ExecuteReader();
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        var product = (string)reader["ProductName"];
+                        Console.WriteLine(product);
+                    }
+                }
+            }
+        }
+    }
+}
