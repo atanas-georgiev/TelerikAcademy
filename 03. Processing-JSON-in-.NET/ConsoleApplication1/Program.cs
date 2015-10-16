7ad11f0d47fdb1ea0a3a95cfed871640a6d0361e
atanas-georgiev <atanasgeorgiev83@gmail.com>
2015-10-16 10:37:06 +0300

    Homework start.

---
+++ 03. Processing-JSON-in-.NET/ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="my company">
+//   Test
+// </copyright>
+// <summary>
+//   The program.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ConsoleApplication1
+{
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void Main(string[] args)
+        {
+        }
+
+        private void test()
+        {
+            var i = 0;
+        }
+    }
+}
---
+++ 11. Entity Framework/Entity-Framework-Homework/Task3-Filter-Orders/Program.cs
@@ -0,0 +1,69 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The program.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Task3_Filter_Orders
+{
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    ///     The program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The query.
+        /// </summary>
+        private const string SqlQuery =
+            "SELECT DISTINCT c.ContactName " + "FROM Customers c " + "INNER JOIN Orders o " + "ON c.CustomerID=o.CustomerID "
+            + "WHERE YEAR(o.OrderDate) = 1997 AND o.ShipCountry = 'Canada'";
+
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Customers who have orders made in 1997 and shipped to Canada.");
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("Entity request:");
+            Console.WriteLine("--------------------------------------------------------------");
+            using (var db = new NORTHWNDEntities())
+            {
+                //var customers = (from c in db.Customers
+                //                 join o in db.Orders on c.CustomerID equals o.CustomerID
+                //                 where o.OrderDate.Value.Year == 1997 && o.ShipCountry == "Canada"
+                //                 select c).ToList();
+
+                var customers = db.Customers.Where(c => c.Orders.Any(x => x.OrderDate != null && (x.OrderDate.Value.Year == 1997 && x.ShipCountry == "Canada"))).ToList(); 
+
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine(customer.ContactName);
+                }
+            }
+
+
+
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("Sql request:");
+            Console.WriteLine("--------------------------------------------------------------");
+            using (var db = new NORTHWNDEntities())
+            {
+                var customers = db.Database.SqlQuery<string>(SqlQuery).ToList();
+
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine(customer);
+                }
+            }
+        }
+    }
+}
