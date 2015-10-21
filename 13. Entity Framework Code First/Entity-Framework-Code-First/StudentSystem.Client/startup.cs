b77c3b67ccf939bed7f424c8e58b0620c919ba70
Atanas Georgiev <atanasgeorgiev83@gmail.com>
2015-10-21 21:18:38 +0300

    Homework updated.

---
+++ 13. Entity Framework Code First/Entity-Framework-Code-First/StudentSystem.Client/startup.cs
@@ -0,0 +1,16 @@
+﻿namespace StudentSystem.Client
+{
+    using System.Data.Entity;
+    using StudentSystem.Data;
+    using StudentSystem.Data.Migrations;
+
+    internal class Startup
+    {
+        internal static void Main()
+        {
+            var db = new StudentSystemContext();
+            db.Database.Delete();
+            db.Database.Create();
+        }
+    }
+}
---
+++ 13. Entity Framework Code First/Entity-Framework-Code-First/StudentSystem.Models/Course.cs
@@ -0,0 +1,32 @@
+﻿namespace StudentSystem.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Course
+    {
+        private ICollection<Student> students;
+
+        public Course()
+        {
+            this.students = new HashSet<Student>();
+        }
+
+        public int Id { get; set; }
+
+        [MinLength(2)]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MinLength(20)]
+        [MaxLength(1000)]
+        public string Description { get; set; }
+
+        public virtual ICollection<Student> Students
+        {
+            get { return this.students; }
+            set { this.students = value; }
+        }
+    }
+}
+
