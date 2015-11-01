57e097b0cb6aea56509d40d72fca872a8554835f
Atanas Georgiev <atanasgeorgiev83@gmail.com>
2015-11-01 18:08:32 +0200

    Homework completed.

--- 02. Linear-Data-Structures/Linear-Data-Structures/Task10/Program.cs
+++ 02. Linear-Data-Structures/Linear-Data-Structures/Task10/Program.cs
@@ -1,8 +1,12 @@
 ﻿namespace Task10
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             //We are given numbers N and M and the following operations:
 
@@ -15,6 +19,45 @@
             //Hint: use a queue.
             //Example: N = 5, M = 16
             //Sequence: 5 → 7 → 8 → 16
+
+            int n = 5;
+            int m = 120;
+
+            var shortestSequance = ShortestSequance(n, m);
+            Console.WriteLine(string.Join(", ", shortestSequance));
+        }
+
+        public static List<int> ShortestSequance(int start, int end)
+        {
+            Stack<int> sequance = new Stack<int>();
+            int currentNumber = end;
+            sequance.Push(currentNumber);
+
+            while (currentNumber > start)
+            {
+                if (currentNumber % 2 == 0 && currentNumber / 2 >= start)
+                {
+                    currentNumber /= 2;
+                    sequance.Push(currentNumber);
+                }
+                else if (currentNumber % 2 == 1 && currentNumber / 2 >= start)
+                {
+                    currentNumber -= 1;
+                    sequance.Push(currentNumber);
+                }
+                else if (currentNumber - 2 >= start)
+                {
+                    currentNumber -= 2;
+                    sequance.Push(currentNumber);
+                }
+                else if (currentNumber - 1 >= start)
+                {
+                    currentNumber -= 1;
+                    sequance.Push(currentNumber);
+                }
+            }
+
+            return sequance.ToList();
         }
     }
 }
