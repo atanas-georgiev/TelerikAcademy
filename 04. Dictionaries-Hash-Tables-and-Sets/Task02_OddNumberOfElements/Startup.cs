9244f1ef404e3d8aeb2f0e7e78d6e4809a609c8c
Atanas Georgiev <atanasgeorgiev83@gmail.com>
2015-11-03 21:55:02 +0200

    Homework started.

---
+++ 04. Dictionaries-Hash-Tables-and-Sets/Task02_OddNumberOfElements/Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace Task02_OddNumberOfElements
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            // Write a program that extracts from a given sequence of strings all elements that present in it odd number of times.Example:
+            // { C#, SQL, PHP, PHP, SQL, SQL } -> {C#, SQL}
+            var inputElements = new[] { "C#", "SQL", "PHP", "PHP", "SQL", "SQL" };
+
+            var countDict = new Dictionary<string, int>();
+
+            foreach (var input in inputElements)
+            {
+                if (!countDict.ContainsKey(input))
+                {
+                    countDict[input] = 1;
+                }
+                else
+                {
+                    countDict[input] += 1;
+                }
+            }
+
+            var result = countDict.Where(x => (x.Value % 2 == 1)).Select(y => y.Key).ToList();
+            Console.WriteLine("Elements presented odd number of times: " + string.Join(", ", result));
+        }
+    }
+}
---
+++ 04. Dictionaries-Hash-Tables-and-Sets/Task03_CountWordsInFile/Startup.cs
@@ -0,0 +1,49 @@
+﻿namespace Task03_CountWordsInFile
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class Startup
+    {
+        private const string FileName = "words.txt";
+
+        public static void Main()
+        {
+            string text = null;
+
+            try
+            {
+                text = File.ReadAllText(FileName);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            if (text != null)
+            {
+                var words = text.Split(new char[] { ' ', ',', '–', '.', '!', '?' }).Where(x => !string.IsNullOrEmpty(x));
+                var countDict = new Dictionary<string, int>();
+
+                foreach (var input in words)
+                {
+                    if (!countDict.ContainsKey(input.ToLower()))
+                    {
+                        countDict[input.ToLower()] = 1;
+                    }
+                    else
+                    {
+                        countDict[input.ToLower()] += 1;
+                    }
+                }
+
+                foreach (var countDictElement in countDict.OrderBy(x => x.Value))
+                {
+                    Console.WriteLine("Element {0} is found {1} times.", countDictElement.Key, countDictElement.Value);
+                }
+            }
+        }
+    }
+}
