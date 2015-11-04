8830a93654bddd0cb9054c5c9e00a9ac41dde8f4
Atanas Georgiev <atanasgeorgiev83@gmail.com>
2015-11-04 21:46:31 +0200

    Homework completed.

---
+++ 04. Dictionaries-Hash-Tables-and-Sets/Task04_HashTable/HashTable.cs
@@ -0,0 +1,140 @@
+﻿namespace Task04_HashTable
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class HashTable<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+    {
+        private const int InitialSize = 16;
+        private const double FillSize = 0.75;
+
+        private int size;
+
+        private LinkedList<KeyValuePair<TKey, TValue>>[] data;
+
+        public HashTable()
+        {
+            this.size = InitialSize;
+            this.data = new LinkedList<KeyValuePair<TKey, TValue>>[this.size];
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.data.Where(x => x != null).Sum(el => el.Count);
+            }
+        }
+
+        public IEnumerable<TKey> Keys
+        {
+            get
+            {
+                var list = new List<TKey>();
+
+                foreach (var el in this.data.Where(x => x != null))
+                {
+                    list.AddRange(el.Select(x => x.Key));
+                }
+
+                return list;
+            }
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                var element = this.data[this.CalculateHashCode(key)];
+
+                if (element == null)
+                {
+                    throw new KeyNotFoundException("Element not found!");
+                }
+
+                return element.First.Value.Value;
+            }
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            var elementToAdd = new KeyValuePair<TKey, TValue>(key, value);
+            var index = this.CalculateHashCode(key);
+
+            if (this.data[index] == null)
+            {
+                this.data[index] = new LinkedList<KeyValuePair<TKey, TValue>>();
+            }
+
+            // Add element in the linked list
+            this.data[index].AddLast(elementToAdd);
+
+            if (this.CountIndexes() >= this.size * FillSize)
+            {
+                this.ResizeData();
+            }
+        }
+
+        public TValue Find(TKey key)
+        {
+            if (this.data[this.CalculateHashCode(key)] == null)
+            {
+                throw new KeyNotFoundException("Element not found!");
+            }
+
+            return this.data[this.CalculateHashCode(key)].Where(x => x.Key.Equals(key)).FirstOrDefault().Value;
+        }
+
+        public void Remove(TKey key)
+        {
+            var element = this.data[this.CalculateHashCode(key)];
+
+            if (element == null)
+            {
+                throw new KeyNotFoundException("Element not found!");
+            }
+
+            this.data[this.CalculateHashCode(key)] = null;
+        }
+
+        public void Clear()
+        {
+            this.size = InitialSize;
+            this.data = new LinkedList<KeyValuePair<TKey, TValue>>[this.size];
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return this.data.Where(x => x != null).SelectMany(element => element).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private int CountIndexes()
+        {
+            return this.data.Count(x => x != null);            
+        }
+
+        private int CalculateHashCode(TKey key)
+        {
+            return key.GetHashCode() % this.size;
+        }
+
+        private void ResizeData()
+        {
+            this.size *= 2;
+            var newData = new LinkedList<KeyValuePair<TKey, TValue>>[this.size];
+
+            foreach (var element in this.data.Where(x => x != null))
+            {
+                newData[this.CalculateHashCode(element.First.Value.Key)] = element;
+            }
+
+            this.data = newData;
+        }
+    }
+}
---
+++ 04. Dictionaries-Hash-Tables-and-Sets/Task05_HashSet/HashSet.cs
@@ -0,0 +1,97 @@
+﻿namespace Task05_Set
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Task04_HashTable;
+
+    public class HashSet<TValue> : IEnumerable<TValue>
+    {
+        private HashTable<int, TValue> data;
+
+        public HashSet()
+        {
+            this.data = new HashTable<int, TValue>();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.data.Count;
+            }
+        }
+
+        public void Add(TValue value)
+        {
+            this.data.Add(value.GetHashCode(), value);
+        }
+
+        public bool Find(TValue value)
+        {
+            var isFound = true;
+
+            try
+            {
+                this.data.Find(value.GetHashCode());
+            }
+            catch (KeyNotFoundException)
+            {
+                isFound = false;
+            }
+            
+            return isFound;            
+        }
+
+        public void Remove(TValue value)
+        {
+            this.data.Remove(value.GetHashCode());
+        }
+
+        public void Clear()
+        {
+            this.data.Clear();
+        }
+
+        public HashSet<TValue> Union(HashSet<TValue> inputSet)
+        {
+            var result = new HashSet<TValue>();
+
+            foreach (var element in this.data)
+            {
+                result.Add(element.Value);
+            }
+
+            foreach (var element in inputSet.Where(element => !result.Find(element)))
+            {
+                result.Add(element);
+            }
+
+            return result;
+        }
+
+        public HashSet<TValue> Intersect(HashSet<TValue> inputSet)
+        {
+            var result = new HashSet<TValue>();
+
+            foreach (var element in this.data.Where(element => inputSet.Find(element.Value)))
+            {
+                result.Add(element.Value);
+            }
+
+            return result;
+        }
+
+        public IEnumerator<TValue> GetEnumerator()
+        {
+            return this.data.Select(element => element.Value).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
