91df3e05523a88be9204f1139501bd7a47dae742
atanas-georgiev <atanasgeorgiev83@gmail.com>
2015-11-12 16:49:42 +0200

    Homework started.

---
+++ 06. Windows-Communication-Foundation/DayOfWeekConsoleClient/Startup.cs
@@ -0,0 +1,17 @@
+﻿namespace DayOfWeekConsoleClient
+{
+    using System;
+    using System.Text;
+
+    using DayOfWeekServiceReference;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var client = new DayOfWeekServiceClient();
+            var result = client.GetDayOfWeekInBulgarian(DateTime.Now);
+            Console.WriteLine(result);
+        }
+    }
+}
