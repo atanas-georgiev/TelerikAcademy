30dad7c86aab173acb1d75f59a0e927829309b21
Atanas Georgiev <atanasgeorgiev83@gmail.com>
2015-11-15 14:35:52 +0200

    Updated homeworks

---
+++ 08. Combinatorics/Task02_Rabbits/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Task02_Rabbits
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        private static Dictionary<int, int> rabbits = new Dictionary<int, int>();
+
+        public static void Main()
+        {
+            var count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                var rabbit = int.Parse(Console.ReadLine());
+
+                if (!rabbits.ContainsKey(rabbit))
+                {
+                    rabbits.Add(rabbit, 1);
+                }
+                else
+                {
+                    rabbits[rabbit] += 1;
+                }                
+            }
+
+            var result = 0;
+
+            foreach (var rabbit in rabbits)
+            {
+                if (rabbit.Value <= rabbit.Key + 1)
+                {
+                    result += rabbit.Key + 1;
+                }
+                else
+                {
+                    result += (rabbit.Key + 1) * (int)((rabbit.Value / (rabbit.Key + 1)) + 1);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Workshop03_SearchAndSort/Task03_Precision/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace Task03_Precision
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            int maxDenominator = int.Parse(Console.ReadLine());
+            var fraction = Console.ReadLine().Split('.')[1];
+
+            int bestNom = 0;
+            int bestDenom = 1;
+            int precision = 0;
+            for (int den = 1; den <= maxDenominator; den++)
+            {
+                int left = 0;
+                int right = den;
+
+                while (left < right)
+                {
+                    int middle = (left + right) / 2;
+
+                    if (Compare(fraction, middle, den))
+                    {
+                        right = middle;
+                    }
+                    else
+                    {
+                        left = middle + 1;
+                    }
+                }
+
+                int current = Divide(fraction, left - 1, den);
+                if (current > precision)
+                {
+                    bestDenom = den;
+                    bestNom = left - 1;
+                    precision = current;
+                }
+
+                current = Divide(fraction, left, den);
+                if (current > precision)
+                {
+                    bestDenom = den;
+                    bestNom = left;
+                    precision = current;
+                }
+
+            }
+
+            Console.WriteLine("{0}/{1}", bestNom, bestDenom);
+            Console.WriteLine(precision + 1);
+        }
+
+        private static int Divide(string fraction, int a, int b)
+        {
+            a *= 10;
+            int i;
+            for (i = 0; i < fraction.Length; i++)
+            {
+                int digit = a / b;
+                if (digit != fraction[i] - '0')
+                {
+                    break;
+                }
+                a = a % b * 10;
+            }
+            return i;
+        }
+
+        private static bool Compare(string fraction, int a, int b)
+        {
+            a *= 10;
+            int i;
+            for (i = 0; i < fraction.Length; i++)
+            {
+                int digit = a / b;
+                if (digit < fraction[i] - '0')
+                {
+                    return false;
+                }
+                else if (digit > fraction[i] - '0')
+                {
+                    return true;
+                }
+
+                a = a % b * 10;
+            }
+            return true;
+        }
+    }
+}
