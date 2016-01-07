b1764ce4b4243399fa032c77eeaf03f360b09d64
atanas-georgiev <atanasgeorgiev83@gmail.com>
2016-01-07 17:26:16 +0200

    Homework complete.

---
+++ 05. View-Engines/View-Engines-Homework/index.js
@@ -0,0 +1,32 @@
+﻿var jadeParser = require('./jade-parser'),
+    data = require('./data')
+    http = require('http'),
+    url = require('url');
+
+http.createServer(function (req, res) {
+    if (req.url === '/') {
+        jadeParser.parse('./templates/template.jade', data.parse.home, function (content) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content, 'utf-8');
+        });
+    } else if (req.url === '/phones') {
+        jadeParser.parse('./templates/template.jade', data.parse.phones, function (content) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content, 'utf-8');
+        });
+    } else if (req.url === '/tablets') {
+        jadeParser.parse('./templates/template.jade', data.parse.tablets, function (content) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content, 'utf-8');
+        });
+    } else if (req.url === '/wearables') {
+        jadeParser.parse('./templates/template.jade', data.parse.wearables, function (content) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content, 'utf-8');
+        });
+    } 
+}).listen(1234);
+
+console.log('Server is running on port ' + 1234)
+
+
---
+++ 05. View-Engines/View-Engines-Homework/jade-parser.js
@@ -0,0 +1,12 @@
+﻿var jade = require('jade'),
+    fs = require('fs');
+
+module.exports.parse = function (templateFile, data, callback) {
+    fs.readFile(templateFile, 'utf-8', function (err, content) {
+        if (err) throw err;
+        var template = jade.compile(content);
+        var output = template(data);
+        
+        callback(output);
+    });
+}
