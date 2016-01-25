af915e21543ea0301846ef6e47d7e222bf1219b6
atanas-georgiev <atanasgeorgiev83@gmail.com>
2016-01-25 15:27:02 +0200

    Homework complete.

---
+++ 11. ASP.NET-Validation-Controls/Validation/Index.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Validation
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CheckInterests();
+        }
+
+        protected void RadioButtonListGender_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CheckInterests();
+        }
+
+        public void CheckInterests()
+        {
+            if (this.RadioButtonListGender.SelectedIndex == 0)
+            {
+                this.PanelMale.Visible = true;
+                this.PanelFemale.Visible = false;
+            }
+            else
+            {
+                this.PanelMale.Visible = false;
+                this.PanelFemale.Visible = true;
+            }
+        }
+    }
+}
