d729bb6c85361d906b11338f2184aca8260c4672
atanas-georgiev <atanasgeorgiev83@gmail.com>
2016-02-05 17:10:17 +0200

    Added skeleton and db models.

---
+++ 04. Working-with-Data/Tweeter.Data.Models/Tag.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tweeter.Data.Models
+{
+    public class Tag
+    {
+        private ICollection<Tweet> tweets;
+
+        public Tag()
+        {
+            this.tweets = new HashSet<Tweet>();
+        }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public ICollection<Tweet> Tweets
+        {
+            get { return this.tweets; }
+            set { this.tweets = value; }
+        }
+    }
+}
---
+++ 04. Working-with-Data/Tweeter.Data.Models/Tweet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tweeter.Data.Models
+{
+    public class Tweet
+    {
+        private ICollection<Tag> tags;
+
+        public Tweet()
+        {
+            this.tags = new HashSet<Tag>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
+        public string Content { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+
+        public ICollection<Tag> Tags
+        {
+            get { return this.tags; }
+            set { this.tags = value; }
+        }
+    }
+}
