419b7239a2744c379543bff90b12121226653526
atanas-georgiev <atanasgeorgiev83@gmail.com>
2016-02-07 11:32:18 +0200

    added skeleton and sample data.

---
+++ 10. Kendo-UI-ASP.NET-MVC-Wrappers/LibrarySystem.Data/LibrarySystemDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using LibrarySystem.Data.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace LibrarySystem.Data
+{
+    public class LibrarySystemDbContext : IdentityDbContext<User>
+    {
+        public LibrarySystemDbContext() 
+            : base("LibrarySystemDb", throwIfV1Schema: false)
+        {
+        }
+
+        public IDbSet<Category> Categorys { get; set; }
+
+        public IDbSet<Book> Books { get; set; }
+
+        public static LibrarySystemDbContext Create()
+        {
+            return new LibrarySystemDbContext();
+        }
+    }
+}
---
+++ 10. Kendo-UI-ASP.NET-MVC-Wrappers/LibrarySystem/App_Start/DatabaseConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using LibrarySystem.Data;
+using LibrarySystem.Data.Migrations;
+
+namespace LibrarySystem.App_Start
+{
+    public class DatabaseConfig
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<LibrarySystemDbContext, Configuration>());
+        }
+    }
+}
