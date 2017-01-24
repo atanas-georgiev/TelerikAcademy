1e85c435cce7e0774dee55eb254288337bbdf258
atanas-georgiev <atanasgeorgiev83@gmail.com>
2017-01-24 08:25:39 +0200

    Renamed CSharp and OOP Repositories

---
+++ exec.bat
@@ -0,0 +1,37 @@
+git fetch https://github.com/atanas-georgiev/HTML-Homework1 master
+git merge -s ours --no-commit --allow-unrelated-histories FETCH_HEAD
+git read-tree --prefix=04.HTML/Homeworks/Homework1 -u FETCH_HEAD
+git commit -m "Merge HTML Homeworks"
+
+git fetch https://github.com/atanas-georgiev/HTML-Homework2 master
+git merge -s ours --no-commit --allow-unrelated-histories FETCH_HEAD
+git read-tree --prefix=04.HTML/Homeworks/Homework2 -u FETCH_HEAD
+git commit -m "Merge HTML Homeworks"
+
+git fetch https://github.com/atanas-georgiev/HTML-Homework3 master
+git merge -s ours --no-commit --allow-unrelated-histories FETCH_HEAD
+git read-tree --prefix=04.HTML/Homeworks/Homework3 -u FETCH_HEAD
+git commit -m "Merge HTML Homeworks"
+
+git fetch https://github.com/atanas-georgiev/HTML-Homework4 master
+git merge -s ours --no-commit --allow-unrelated-histories FETCH_HEAD
+git read-tree --prefix=04.HTML/Homeworks/Homework4 -u FETCH_HEAD
+git commit -m "Merge HTML Homeworks"
+
+git fetch https://github.com/atanas-georgiev/CSS-Homework1 master
+git merge -s ours --no-commit --allow-unrelated-histories FETCH_HEAD
+git read-tree --prefix=05.CSS/Homeworks/Homework1 -u FETCH_HEAD
+git commit -m "Merge CSS Homeworks"
+
+git fetch https://github.com/atanas-georgiev/CSS-Homework2 master
+git merge -s ours --no-commit --allow-unrelated-histories FETCH_HEAD
+git read-tree --prefix=05.CSS/Homeworks/Homework2 -u FETCH_HEAD
+git commit -m "Merge CSS Homeworks"
+
+
+git fetch https://github.com/atanas-georgiev/CSS-Homework3 master
+git merge -s ours --no-commit --allow-unrelated-histories FETCH_HEAD
+git read-tree --prefix=05.CSS/Homeworks/Homework3 -u FETCH_HEAD
+git commit -m "Merge CSS Homeworks"
+
+
